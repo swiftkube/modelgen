b9493de90beef61a2ea5ae336239d730afb3ebe4
Iskandar Abudiab <mail@iabudiab.dev>
2022-12-31 00:52:48 +0100

    Update Swift to v5.5 and update dependencies accordingly

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,12 +6,13 @@ import PackageDescription
 let package = Package(
 	name: "SwiftkubeModelGen",
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
-		.package(url: "https://github.com/stencilproject/Stencil", from: "0.13.1"),
-		.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+		.package(url: "https://github.com/stencilproject/Stencil", from: "0.15.1"),
+		.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0")
 	],
+
 	targets: [
-		.target(
+		.executableTarget(
 			name: "skgen",
 			dependencies: [
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
